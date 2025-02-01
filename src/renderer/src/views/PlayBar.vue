5f1dfc3c941ad2d2f5123a41f27b9062bb3c714a
zhf521 <3407085928@qq.com>
2025-02-01 21:33:23 +0800

    feat: 更新布局

---
+++ src/renderer/src/views/PlayBar.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="w-full h-[80px] bg-gray-300"></div>
+</template>
+<script setup lang="ts"></script>
+<style scoped></style>
--- src/renderer/src/views/SideBar.vue
+++ src/renderer/src/views/SideBar.vue
@@ -1,6 +1,6 @@
 <template>
-    <Layout />
 </template>
 <script setup lang="ts">
-import Layout from './views/Layout.vue'
 </script>
+<style scoped>
+</style>
