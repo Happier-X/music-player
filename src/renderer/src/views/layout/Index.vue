118ba5a4277f60a3f84665c691ebf46979126082
zhf521 <3407085928@qq.com>
2025-02-02 10:47:12 +0800

    feat: 完成布局

---
+++ src/renderer/src/views/layout/Index.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="w-screen h-screen">
+        <div class="flex h-[calc(100vh-80px)] w-full">
+            <SideBar></SideBar>
+            <div class="w-[calc(100%-224px)] h-full">
+                <TopBar></TopBar>
+                <RouterView></RouterView>
+            </div>
+        </div>
+        <PlayBar></PlayBar>
+    </div>
+</template>
+<script setup lang="ts">
+import SideBar from './SideBar.vue'
+import TopBar from './TopBar.vue'
+import PlayBar from './PlayBar.vue'
+</script>
+<style scoped></style>
