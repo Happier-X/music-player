460a79286b26ac412184fd84b11681a42ff17a0f
赵鸿飞 <zhaohongfei@vasoyn.com>
2025-02-17 10:52:40 +0800

    feat: 增加清除按钮

--- src/renderer/src/views/settings/components/MediaLibrary.vue
+++ src/renderer/src/views/settings/components/MediaLibrary.vue
@@ -2,14 +2,14 @@
     <n-form ref="formRef">
         <n-card title="媒体库" :bordered="false">
             <n-form-item label="服务地址" path="inputValue">
-                <n-input placeholder="http://">
+                <n-input placeholder="http://" clearable>
                     <template #prefix>
                         <n-icon :component="ServerIcon" />
                     </template>
                 </n-input>
             </n-form-item>
             <n-form-item label="用户名" path="inputValue">
-                <n-input placeholder="用户名">
+                <n-input placeholder="用户名" clearable>
                     <template #prefix>
                         <n-icon :component="UserIcon" />
                     </template>
@@ -19,7 +19,8 @@
                 <n-input
                     placeholder="密码"
                     type="password"
-                    show-password-on="click">
+                    show-password-on="click"
+                    clearable>
                     <template #prefix>
                         <n-icon :component="PasswordIcon" />
                     </template>
