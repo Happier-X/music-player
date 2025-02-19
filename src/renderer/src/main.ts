5b713138a6df475d0f5178b062a5cd064ec09391
赵鸿飞 <zhaohongfei@vasoyn.com>
2025-02-19 08:59:21 +0800

    chore: 引入daisyUI

--- src/renderer/src/main.ts
+++ src/renderer/src/main.ts
@@ -1,6 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router/index'
-import 'modern-normalize/modern-normalize.css'
+import './assets/main.css'
 
 createApp(App).use(router).mount('#app')
