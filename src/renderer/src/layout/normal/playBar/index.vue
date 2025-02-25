19e07f5d23659e39f661af52a62093eb5003f81d
Happier <zhf5256@qq.com>
2025-02-25 22:46:55 +0800

    fix: 更新音乐页面

--- src/renderer/src/layout/normal/playBar/index.vue
+++ src/renderer/src/layout/normal/playBar/index.vue
@@ -1,5 +1,3 @@
 <template>
-    <div>播放条</div>
 </template>
 <script setup lang="ts"></script>
-<style scoped></style>
--- src/renderer/src/views/songs/Index.vue
+++ src/renderer/src/views/songs/Index.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="w-full h-full">
         <div class="w-full h-[200px]">
-            <div class="card card-side bg-base-100 shadow-sm p-4">
+            <div class="card card-side bg-base-100 p-4">
                 <figure>
                     <div class="avatar flex items-center justify-center">
                         <div class="w-40 h-40 rounded">
@@ -12,30 +12,23 @@
                 </figure>
                 <div class="card-body">
                     <h2 class="card-title">歌曲</h2>
-                    <p>123个</p>
+                    <p>共123首</p>
                     <div class="card-actions justify-start">
-                        <button class="btn btn-neutral">全部播放</button>
-                        <button class="btn btn-neutral">随机播放</button>
+                        <button class="btn btn-neutral">播放全部</button>
                     </div>
                 </div>
             </div>
         </div>
-        <div class="w-full h-[calc(100%-200px)]">
-            <ul class="list bg-base-100 rounded-box shadow-md">
-                <li class="p-4 pb-2 text-xs opacity-60 tracking-wide">
-                    Most played songs this week
-                </li>
-
-                <li class="list-row">
-                    <div class="text-4xl font-thin opacity-30 tabular-nums">
-                        01
-                    </div>
+        <div
+            class="w-full h-[calc(100%-200px)] overflow-y-scroll scrollbar-hide">
+            <ul class="list">
+                <li class="list-row" v-for="(item, index) in 100" :key="index">
                     <div>
                         <img
                             class="size-10 rounded-box"
                             src="https://img.daisyui.com/images/profile/demo/1@94.webp" />
                     </div>
-                    <div class="list-col-grow">
+                    <div>
                         <div>Dio Lupa</div>
                         <div class="text-xs uppercase font-semibold opacity-60">
                             Remaining Reason
@@ -56,23 +49,6 @@
                             </g>
                         </svg>
                     </button>
-                </li>
-
-                <li class="list-row">
-                    <div class="text-4xl font-thin opacity-30 tabular-nums">
-                        02
-                    </div>
-                    <div>
-                        <img
-                            class="size-10 rounded-box"
-                            src="https://img.daisyui.com/images/profile/demo/4@94.webp" />
-                    </div>
-                    <div class="list-col-grow">
-                        <div>Ellie Beilish</div>
-                        <div class="text-xs uppercase font-semibold opacity-60">
-                            Bears of a fever
-                        </div>
-                    </div>
                     <button class="btn btn-square btn-ghost">
                         <svg
                             class="size-[1.2em]"
@@ -84,39 +60,8 @@
                                 stroke-width="2"
                                 fill="none"
                                 stroke="currentColor">
-                                <path d="M6 3L20 12 6 21 6 3z"></path>
-                            </g>
-                        </svg>
-                    </button>
-                </li>
-
-                <li class="list-row">
-                    <div class="text-4xl font-thin opacity-30 tabular-nums">
-                        03
-                    </div>
-                    <div>
-                        <img
-                            class="size-10 rounded-box"
-                            src="https://img.daisyui.com/images/profile/demo/3@94.webp" />
-                    </div>
-                    <div class="list-col-grow">
-                        <div>Sabrino Gardener</div>
-                        <div class="text-xs uppercase font-semibold opacity-60">
-                            Cappuccino
-                        </div>
-                    </div>
-                    <button class="btn btn-square btn-ghost">
-                        <svg
-                            class="size-[1.2em]"
-                            xmlns="http://www.w3.org/2000/svg"
-                            viewBox="0 0 24 24">
-                            <g
-                                stroke-linejoin="round"
-                                stroke-linecap="round"
-                                stroke-width="2"
-                                fill="none"
-                                stroke="currentColor">
-                                <path d="M6 3L20 12 6 21 6 3z"></path>
+                                <path
+                                    d="M19 14c1.49-1.46 3-3.21 3-5.5A5.5 5.5 0 0 0 16.5 3c-1.76 0-3 .5-4.5 2-1.5-1.5-2.74-2-4.5-2A5.5 5.5 0 0 0 2 8.5c0 2.3 1.5 4.05 3 5.5l7 7Z"></path>
                             </g>
                         </svg>
                     </button>
