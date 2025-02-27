d5f74c9d7b9eb9b3fe88568f4439391714cd3549
赵鸿飞 <zhaohongfei@vasoyn.com>
2025-02-27 15:38:48 +0800

    fix: 引入搜索接口

--- src/renderer/src/api/subsonic.ts
+++ src/renderer/src/api/subsonic.ts
@@ -273,26 +273,9 @@ export const subsonicApi = {
     //             musicFolderId
     //         }
     //     }),
-    // /**
-    //  * 搜索专辑、歌曲、艺术家
-    //  * @param {Object} params 搜索参数
-    //  * @param {string} params.query 搜索关键词
-    //  * @param {number} [params.artistCount=20] 可选的，返回的艺术家数量，默认20
-    //  * @param {number} [params.artistOffset=0] 可选的，艺术家列表偏移量，用于分页，默认0
-    //  * @param {number} [params.albumCount=20] 可选的，返回的专辑数量，默认20
-    //  * @param {number} [params.albumOffset=0] 可选的，专辑列表偏移量，用于分页，默认0
-    //  * @param {number} [params.songCount=20] 可选的，返回的歌曲数量，默认20
-    //  * @param {number} [params.songOffset=0] 可选的，歌曲列表偏移量，用于分页，默认0
-    //  * @param {string} [params.musicFolderId] 可选的，音乐文件夹ID
-    //  */
-    // search: (params) =>
-    //     request({
-    //         url: '/rest/search2',
-    //         params
-    //     }),
     /**
-     * 搜索专辑、歌曲、艺术家（根据 ID3 标签组织）
-     * @param {Object} params 参数
+     * 搜索专辑、歌曲、艺术家
+     * @param {Object} params 搜索参数
      * @param {string} params.query 搜索关键词
      * @param {number} [params.artistCount=20] 可选的，返回的艺术家数量，默认20
      * @param {number} [params.artistOffset=0] 可选的，艺术家列表偏移量，用于分页，默认0
@@ -302,11 +285,28 @@ export const subsonicApi = {
      * @param {number} [params.songOffset=0] 可选的，歌曲列表偏移量，用于分页，默认0
      * @param {string} [params.musicFolderId] 可选的，音乐文件夹ID
      */
-    searchOrganizedByID3: (params) =>
+    search: (params) =>
         request({
-            url: '/rest/search3',
+            url: '/rest/search2',
             params
         }),
+    // /**
+    //  * 搜索专辑、歌曲、艺术家（根据 ID3 标签组织）
+    //  * @param {Object} params 参数
+    //  * @param {string} params.query 搜索关键词
+    //  * @param {number} [params.artistCount=20] 可选的，返回的艺术家数量，默认20
+    //  * @param {number} [params.artistOffset=0] 可选的，艺术家列表偏移量，用于分页，默认0
+    //  * @param {number} [params.albumCount=20] 可选的，返回的专辑数量，默认20
+    //  * @param {number} [params.albumOffset=0] 可选的，专辑列表偏移量，用于分页，默认0
+    //  * @param {number} [params.songCount=20] 可选的，返回的歌曲数量，默认20
+    //  * @param {number} [params.songOffset=0] 可选的，歌曲列表偏移量，用于分页，默认0
+    //  * @param {string} [params.musicFolderId] 可选的，音乐文件夹ID
+    //  */
+    // searchOrganizedByID3: (params) =>
+    //     request({
+    //         url: '/rest/search3',
+    //         params
+    //     }),
     // /**
     //  * 根据用户名获取歌单
     //  */
